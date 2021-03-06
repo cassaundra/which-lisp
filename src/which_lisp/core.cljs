fa7b567c8a123de515fe482f0a5c2c510dbbd07c
Cassaundra <cass@cassaundra.io>
2021-03-05 17:11:02 -0800

    Add basic site content and CSV table renderer

--- src/which_lisp/core.cljs
+++ src/which_lisp/core.cljs
@@ -1,16 +1,78 @@
 (ns which-lisp.core
-    (:require
-     [which-lisp.style :as s]
-     [reagent.core :as r]
-     [reagent.dom :as d]
-     [stylefy.core :as stylefy :refer [use-style]]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require
+   [cljs-http.client :as http]
+   [cljs.core.async :refer [<!]]
+   [clojure.string :as str]
+   [reagent.core :as r]
+   [reagent.dom :as d]
+   [stylefy.core :as stylefy :refer [use-style]]
+   [testdouble.cljs.csv :as csv]
+   [which-lisp.style :as s]))
+
+(def data (r/atom nil))
+
+(defn read-data []
+  (go (let [response (<! (http/get "data.csv"))]
+        (reset! data (:body response)))))
+
+(defn language-table []
+  (when @data
+    [:table.table
+     [:thead
+      [:tr
+       (for [col '("Language" "Type" "Target(s)" "Implementation(s)" "Features" "Status" "License")]
+         [:th {:scope "col"} col])]]
+     [:tbody
+      ;; TODO add keys for performance
+      (for [entry (csv/read-csv (str/trim @data) :separator "|")]
+        [:tr
+         (let [cols
+           (let [[lang url type target impl description features status license] entry]
+             (list [:a {:href url :target "blank"} lang]
+                   (case type
+                     "cl" "Common Lisp-like"
+                     "clj" "Clojure-like"
+                     "extra" "Extra semantics"
+                     "scm" "Scheme-like"
+                     "sexp" "S-exp mapping"
+                     "Unknown")
+                   target
+                   impl
+                   [:span (use-style {:white-space "pre-wrap"})
+                    description "\n"
+                    (for [feature (str/split features " ")]
+                      [:span [:span {:class "badge rounded-pill bg-secondary"} feature] " "])]
+                   status
+                   license
+                   ))]
+           (for [col cols]
+             ;; ^{:key col}
+             [:td {:scope "row"} col]))]
+        ;; TODO how
+
+        )]])
+  )
 
 (defn home-page []
-  [:div [:h1 "Which Lisp?"]])
+  [:div.container
+   [:h1 "Which Lisp?"]
+   [:p "Lisp is great, but there are so many dialects! How do you choose?"]
+   [:p
+    "This tool is designed to help those interested in learning to use a lisp-flavored "
+    "language, but don't know where to start."]
+   [:p "Special thanks to the contributors of "
+    [:a {:href "https://github.com/dundalek/awesome-lisp-languages" :target "blank"}
+     "awesome-lisp-languages"]
+    ", from which much of the information on this site originates."
+    ]
+   [:h2 "Table"]
+   [language-table]])
 
 (defn mount-root []
   (s/constant-styles)
-  (d/render [home-page] (.getElementById js/document "app")))
+  (d/render [home-page] (.getElementById js/document "app"))
+  (read-data))
 
 (defn ^:export init! []
   (mount-root))
--- src/which_lisp/style.cljs
+++ src/which_lisp/style.cljs
@@ -4,22 +4,13 @@
 
 (s/init)
 
-(def text-style
-  {:background-color "red"})
 (defn constant-styles []
   (s/tag "body"
-         {:font-family "'Helvetica Neue', Verdana, Helvetica, Arial, sans-serif"
-          :max-width "600px"
+         {
           :margin "0 auto"
           :padding-top "72px"
-          :-webkit-font-smoothing "antialiased"
-          :font-size "1.125em"
-          :color "#333"
-          :line-height "1.5em"})
-  (s/tag "h1, h2, h3" {:color "#000"})
-  (s/tag "h1" {:font-size "2.5em"})
-  (s/tag "h2" {:font-size "2em"})
-  (s/tag "h3" {:font-size "1.5em"})
-  (s/tag "a" {:text-decoration "none"
-              :color "#09f"})
-  (s/tag "a:hover" {:text-decoration "underline"}))
+          ;; :-webkit-font-smoothing "antialiased"
+          ;; :font-size "1.125em"
+          ;; :color "#333"
+          ;; :line-height "1.5em"
+          }))
